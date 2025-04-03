f442d87d14cfb429e5cc4fd55926ea8a95f30c77
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-04-03 15:55:46 -0700

    Start compiling 2021-2024 genetic data

---
+++ R/kitsumkalum.R
@@ -0,0 +1,14 @@
+#' Kitsumkalum escapement data by year
+#'
+#' Kitsumkalum escapement data by year
+#'
+#'
+#' @format ## `kitsumkalum`
+#' A dataframe with three variables:
+#' \describe{
+#'   \item{year}{Year}
+#'   \item{kitsumkalum_escapement}{Escapement of Kitsumkalum Chinook ages 4-7, no jacks, estimated using mark-recapture and POPAN.}
+#'   \item{sd}{Standard Deviation of kitsumkalum_escapement.}
+#' }
+#' @source data-raw/process-kitsumkalum.R, data-raw/kitsumkalum-escapement.csv
+"kitsumkalum"
