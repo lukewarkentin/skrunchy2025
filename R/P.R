ce18f1c7c07582cccee62012f5794456d6a88454
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-04-04 13:18:21 -0700

    Add incomplete P and sigma-P data in data/ and documentation for these data sets in R/

---
+++ R/P.R
@@ -0,0 +1,15 @@
+#' Genetic proportion data
+#'
+#' Genetic proportion data
+#'
+#'
+#' @format ## `P`
+#' An array with three dimensions:
+#' \describe{
+#'   \item{i}{Population}
+#'   \item{w}{Week}
+#'   \item{y}{Year}
+#' }
+#' @source data-raw/process-P.R
+"P"
+
---
+++ R/sigma-P.R
@@ -0,0 +1,15 @@
+#' Standard error for genetic proportion data
+#'
+#' Standard error of genetic proportion data by population, week, and year.
+#'
+#'
+#' @format ## `sigma_P`
+#' An array with three dimensions:
+#' \describe{
+#'   \item{i}{Population}
+#'   \item{w}{Week}
+#'   \item{y}{Year}
+#' }
+#' @source data-raw/process-P.R
+"sigma_P"
+
