d1b0132ee098a72dab3700b9eb2649584a801e0e
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-05-07 09:28:30 -0700

    Include data/ folder in repo, add .rda files

---
+++ R/process-B.R
@@ -0,0 +1,14 @@
+# Create rda file for brood removals
+
+library(here)
+library(skrunchy)
+
+
+d <- read.csv(here("data-raw/brood-removals.csv"))
+
+B_star <- df_to_array(d, value= "brood_removals", dimnames_order = c("y", "a"), FUN = sum, default = 0)
+
+
+
+
+usethis::use_data(B_star, overwrite = TRUE)
