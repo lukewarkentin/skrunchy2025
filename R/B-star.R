36bf3045fc90d3d72cca51f62d5ee1c964ecb7fd
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-05-14 12:03:20 -0700

    Update readme

---
+++ R/B-star.R
@@ -0,0 +1,14 @@
+#' Female brood removals by age
+#'
+#' Brood removals in the Kitsumkalum River, females only, by age
+#'
+#'
+#' @format ## `B_star`
+#' An array with two dimensions:
+#' \describe{
+#'   \item{y}{Year}
+#'   \item{a}{Age}
+#' }
+#' @source data-raw/process-B-star.R
+"P"
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # skrunchy2025
 Update Skeena River run reconstruction for Chinook salmon, with data available in 2025
+
+Uses the skrunchy package https://github.com/Pacific-salmon-assess/skrunchy 
