b5bfd48a3f9c8006f9a6b8c81588a63ea1dfa378
lukewarkentin <luke.warkentin@gmail.com>
2026-05-25 10:51:44 -0700

    Re run run recon, filter brood removals to 2024 and earlier, move fig script, remove do-run-recon-rru script from R folder, move to data-raw folder.

--- data-raw/kitsumkalum/process-B-star.R
+++ data-raw/kitsumkalum/process-B-star.R
@@ -1,14 +1,19 @@
-B# Create rda file for brood removals
+# Create rda file for brood removals
 
 library(here)
 library(skrunchy)
 
 
-d <- read.csv(here("data-raw/brood-removals.csv"))
+d <- read.csv(here("data-raw/kitsumkalum/brood-removals.csv"))
+d <- d[d$y <= 2024, ]
 
-B_star <- df_to_array(d, value= "brood_removals", dimnames_order = c("y", "a"), FUN = sum, default = 0)
-
-
+B_star <- df_to_array(
+  d,
+  value = "brood_removals",
+  dimnames_order = c("y", "a"),
+  FUN = sum,
+  default = 0
+)
 
 
 usethis::use_data(B_star, overwrite = TRUE)
